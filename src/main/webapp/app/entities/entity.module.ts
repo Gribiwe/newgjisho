b9196f4cdeb0e7b12ebfd50e1d83d5f694472ba0
nick.g <nick.g@implemica.com>
2019-05-14 00:09:15 +0300

    initial

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,30 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'user-record',
+        loadChildren: './user-record/user-record.module#GjishoUserRecordModule'
+      },
+      {
+        path: 'answer',
+        loadChildren: './answer/answer.module#GjishoAnswerModule'
+      },
+      {
+        path: 'user-record-variable',
+        loadChildren: './user-record-variable/user-record-variable.module#GjishoUserRecordVariableModule'
+      },
+      {
+        path: 'record-list',
+        loadChildren: './record-list/record-list.module#GjishoRecordListModule'
+      },
+      {
+        path: 'tag',
+        loadChildren: './tag/tag.module#GjishoTagModule'
+      },
+      {
+        path: 'user-data',
+        loadChildren: './user-data/user-data.module#GjishoUserDataModule'
+      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ],
